61a7d5c2a93c4eb028cfab08ae718339ef66b864
val-marinho <ea.valesca.hutcherson@gmail.com>
2023-01-25 22:48:37 -0300

    atualizacao da pagina

--- index.js
+++ index.js
@@ -12,3 +12,40 @@ for(let i = 0; i < pergunta.length; i++){
         }
     })
 }
+
+function over1 ( )  {
+    document . getElementById ( "vicky" ) . src  =  './img/vicky_color.jpeg' ;
+}
+function  out1 ( )  {
+    document . getElementById ( "vicky" ) . src  =  './img/vick_pretoebranco.jpeg' ;
+}
+
+//
+
+function  over2 ( )  {
+    document . getElementById ( "val" ) . src  =  './img/val_color.jpeg' ;
+}
+function out2 ( )  {
+    document . getElementById ( "val" ) . src  =  './img/val_pretoebranco.jpeg' ;
+}
+
+//
+
+function  over3 ( )  {
+    document . getElementById ( "bea" ) . src  =  './img/bea_color.jpeg' ;
+}
+function out3 ( )  {
+    document . getElementById ( "bea" ) . src  =  './img/bea_pretoebranco.jpeg' ;
+}
+
+//
+
+function over4 ( )  {
+    document . getElementById ( "matheus" ) . src  =  './img/matheus_color.jpg' ;
+}
+function  out4 ( )  {
+    document . getElementById ( "matheus" ) . src  =  './img/matheus_pretoebranco.jpeg' ;
+}
+
+
+
